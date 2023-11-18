476dba6ab2597d28a98e9b37860755219e943f85
mrrsmburu <mrrsmburu@gmail.com>
2023-11-18 14:30:21 +0300

    completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+
+
+  const App = () => {
+    const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchQuestions = async () => {
+        try {
+        
+          setLoading(true);
+  
+          
+          const response = await fetch('https://example.com/api/questions');
+          const data = await response.json();
+  
+      
+          setQuestions(data.questions);
+        } catch (error) {
+          console.error('Error fetching questions:', error);
+        } finally {
+      
+          setLoading(false);
+        }
+      };
+  
+      
+      fetchQuestions();
+    }, []); 
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{}</ul>
     </section>
   );
 }
